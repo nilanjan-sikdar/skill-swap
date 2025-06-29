c87906ff202f97f77edcda40fc80812343c73f13
Nilanjan Sikdar <nilanjansikdar66@gmail.com>
2025-06-29 10:13:08 +0530

    Delete Desktop/skills-swaper-main (1)/skills-swaper-main directory

--- Desktop/skills-swaper-main (1)/skills-swaper-main/src/routes/discussionRoutes.tsx
+++
@@ -1,8 +0,0 @@
-
-import { Route } from "react-router-dom";
-import DiscussionDetail from "@/pages/DiscussionDetail";
-
-// This file exports discussion-related routes that can be imported in App.tsx
-export const discussionRoutes = (
-  <Route path="/discussions/:id" element={<DiscussionDetail />} />
-);
